21544cd6e9702dc1a2e1da343b1bb34b15e652fb
Bonnie Hurwitz <bhurwitz@arizona.edu>
2024-01-08 16:42:43 -0700

    repo update for pynb

---
+++ assignments/00_getting_started/test.py
@@ -0,0 +1,23 @@
+""" Tests for howdy.py """
+
+import os
+from subprocess import getstatusoutput
+
+PRG = './howdy.py'
+
+
+# --------------------------------------------------
+def test_exists():
+    """ Program exists """
+
+    assert os.path.isfile(PRG)
+
+
+# --------------------------------------------------
+def test_usage():
+    """ Prints usage """
+
+    for flag in ['-h', '--help']:
+        rv, out = getstatusoutput(f'{PRG} {flag}')
+        assert rv == 0
+        assert out.lower().startswith('usage')
